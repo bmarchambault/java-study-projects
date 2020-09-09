770e37a1e78ab5c1a5f014dd7b6e745dbb8c22f8
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-09 14:17:33 -0500

    created a random password generator method

--- src/emailAdminApp/Email.java
+++ src/emailAdminApp/Email.java
@@ -9,6 +9,7 @@ public class Email {
     private String department;
     private int mailboxCapacity;
     private String alternateEmail;
+    private int defaultPasswordLength;
 
     public Email (){};
 
@@ -21,6 +22,10 @@ public class Email {
         this.department = setDepartment();
         //RETURN A RESPONSE:
         System.out.println("Department: " + this.department);
+
+        //CALL A METHOD THAT RETURNS A RANDOM PASSWORD:
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Your password is: " + this.password);
     }
 
 //    ASK FOR DEPARTMENT
@@ -40,7 +45,24 @@ public class Email {
     }
 
     //random password generator:
+private String randomPassword(int length){
+        //int length will determine how long our password needs to be
+    //defining which characters to choose from to generate a password:
+    //this is the array of characters to iterate through
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*";
+        //this is the password.  a new sequence of characters of the size of length.
+    char[] password = new char[length];
+    //iteration as many times as the length of the password needs to be - determined by the parameter
+    for(int i = 0; i<length; i++){
+        //math.random is 0-1 * the number of characters in passwordSet
+        int random = (int) (Math.random() * passwordSet.length());
+        //password character at ith iteration equals a random character from passwordSet
+        password[i] = passwordSet.charAt(random);
+    }
+    //casting a single character into a string of characters:
+return new String (password);
 
+}
 
     public String getFirstName() {
         return firstName;
