cf4541a6ab9b016ab4bf1b0b3267dd8ef7e57cc3
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-09 13:55:37 -0500

    commented out code so I can work on email app

--- src/branceProjectFiles/ArrayListsAndLoops/ArrayListPractice.java
+++ src/branceProjectFiles/ArrayListsAndLoops/ArrayListPractice.java
@@ -1,81 +1,81 @@
-package branceProjectFiles.ArrayListsAndLoops;
-
-import OOPReview.User;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class ArrayListPractice {
-
-    // empty so far []
-    private static ArrayList<String> colors = new ArrayList<>();
-    private static ArrayList<User> userList = new ArrayList<>();
-    private static HashMap<String, User> userMap = new HashMap<>();
-
-    public static void main(String[] args) {
-        // .add is a method to put new elements into the ArrayList
-
-        colors.add("green");
-        // ["green"]
-
-        // Anything added will be put at the end of the ArrayList
-
-        colors.add("blue");
-        //["green", "blue"]
-
-        // ArrayLists let us add to the list at specific indexes
-        // If I want to add red in between green and blue I add an int to specify what index I want to add the new element
-        // The .add method is overloaded so if you use 2 parameters you use the version that adds to a specific index
-        // You pass an integer first for the index you want to put the element
-        // You pass what you want to add to the ArrayList as the second argument
-        // We put 1 as the index point. Blue is currently at index 1.
-        // Since we said that red goes at index 1 then red goes to index 1 and blue is moved to the next index
-
-        colors.add(1, "red");
-        // ["green", "red", "blue"]
-
-
-        // Normally we use [index] to access specific elements
-        // ArrayLists are an object that has methods that manipulate an array behind the scenes
-        // Instead of colors[1] to access the String "red" we use colors.get(1)
-
-        String oneColor = colors.get(1);
-        // oneColor = "red"
-        // colors = ["green", "red", "blue"]
-
-        // In Javascript we used pop and shift to remove elements from an array
-        // ArrayLists use the .remove()
-
-        // We pass .remove an index number and it will remove the element at that index
-//        colors.remove(2);
-        // colors = ["green", "red"]
-
-
-        // .remove is overloaded so you can remove the an element the first time the element is found
-//        colors.remove("red");
-        // colors = ["green"]
-
-        User brance = new User("javamancer", "brance@codeup.com", "abc123");
-        User jasmine = new User("mirafaun", "jasmine@gmail.com", "def456");
-
-        userMap.put("branceKey", brance);
-        userMap.put("jasmineKey", jasmine);
-
-        userList.add(brance);
-        userList.add(jasmine);
-
-
-//        for(int i = 0; i < colors.size() - 1; i++){
-//            System.out.println(colors.get(i));
+//package branceProjectFiles.ArrayListsAndLoops;
+//
+//import OOPReview.User;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//
+//public class ArrayListPractice {
+//
+//    // empty so far []
+//    private static ArrayList<String> colors = new ArrayList<>();
+//    private static ArrayList<User> userList = new ArrayList<>();
+//    private static HashMap<String, User> userMap = new HashMap<>();
+//
+//    public static void main(String[] args) {
+//        // .add is a method to put new elements into the ArrayList
+//
+//        colors.add("green");
+//        // ["green"]
+//
+//        // Anything added will be put at the end of the ArrayList
+//
+//        colors.add("blue");
+//        //["green", "blue"]
+//
+//        // ArrayLists let us add to the list at specific indexes
+//        // If I want to add red in between green and blue I add an int to specify what index I want to add the new element
+//        // The .add method is overloaded so if you use 2 parameters you use the version that adds to a specific index
+//        // You pass an integer first for the index you want to put the element
+//        // You pass what you want to add to the ArrayList as the second argument
+//        // We put 1 as the index point. Blue is currently at index 1.
+//        // Since we said that red goes at index 1 then red goes to index 1 and blue is moved to the next index
+//
+//        colors.add(1, "red");
+//        // ["green", "red", "blue"]
+//
+//
+//        // Normally we use [index] to access specific elements
+//        // ArrayLists are an object that has methods that manipulate an array behind the scenes
+//        // Instead of colors[1] to access the String "red" we use colors.get(1)
+//
+//        String oneColor = colors.get(1);
+//        // oneColor = "red"
+//        // colors = ["green", "red", "blue"]
+//
+//        // In Javascript we used pop and shift to remove elements from an array
+//        // ArrayLists use the .remove()
+//
+//        // We pass .remove an index number and it will remove the element at that index
+////        colors.remove(2);
+//        // colors = ["green", "red"]
+//
+//
+//        // .remove is overloaded so you can remove the an element the first time the element is found
+////        colors.remove("red");
+//        // colors = ["green"]
+//
+//        User brance = new User("javamancer", "brance@codeup.com", "abc123");
+//        User jasmine = new User("mirafaun", "jasmine@gmail.com", "def456");
+//
+//        userMap.put("branceKey", brance);
+//        userMap.put("jasmineKey", jasmine);
+//
+//        userList.add(brance);
+//        userList.add(jasmine);
+//
+//
+////        for(int i = 0; i < colors.size() - 1; i++){
+////            System.out.println(colors.get(i));
+////        }
+//
+//        for(String placeholder : colors){
+//            System.out.println(placeholder);
 //        }
-
-        for(String placeholder : colors){
-            System.out.println(placeholder);
-        }
-
-        for(User placeholder : userList){
-            System.out.println("placeholder.getUsername() = " + placeholder.getUsername());
-        }
-
-    }
-}
+//
+//        for(User placeholder : userList){
+//            System.out.println("placeholder.getUsername() = " + placeholder.getUsername());
+//        }
+//
+//    }
+//}
--- src/branceProjectFiles/mystore/Cashier.java
+++ src/branceProjectFiles/mystore/Cashier.java
@@ -1,19 +1,19 @@
-package branceProjectFiles.mystore;
-
-import peoplestuff.Person;
-
-import java.util.List;
-
-public class Cashier extends Person {
-    public Cashier(String newPersonFirstName, String newPersonLastName, int newPersonAge) {
-        super(newPersonFirstName, newPersonLastName, newPersonAge);
-    }
-
-    public double total(List<Product> cart) {
-        double sum = 0;
-        for (Product item : cart){
-            sum += item.getPrice();//at first is says variable sum might not have been initialized.  make it equal something first. if you want a variable to change in a loop, you need to give it an initial value first.
-        }
-        return sum;
-    }
-}
+//package branceProjectFiles.mystore;
+//
+//import peoplestuff.Person;
+//
+//import java.util.List;
+//
+//public class Cashier extends Person {
+//    public Cashier(String newPersonFirstName, String newPersonLastName, int newPersonAge) {
+//        super(newPersonFirstName, newPersonLastName, newPersonAge);
+//    }
+//
+//    public double total(List<Product> cart) {
+//        double sum = 0;
+//        for (Product item : cart){
+//            sum += item.getPrice();//at first is says variable sum might not have been initialized.  make it equal something first. if you want a variable to change in a loop, you need to give it an initial value first.
+//        }
+//        return sum;
+//    }
+//}
