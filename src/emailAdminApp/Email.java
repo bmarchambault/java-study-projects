61fc12d59156f54f1585f43d680b563b90a6e6fe
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-09 13:55:05 -0500

    created a scanner that reads user input.  created a method that assigns a department

--- src/emailAdminApp/Email.java
+++ src/emailAdminApp/Email.java
@@ -1,5 +1,7 @@
 package emailAdminApp;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -14,6 +16,27 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("Email created: " + this.firstName + " " + this.lastName);
+
+        //CALL A METHOD THAT ASKS FOR THE DEPARTMENT - RETURNS THE DEPARTMENT
+        this.department = setDepartment();
+        //RETURN A RESPONSE:
+        System.out.println("Department: " + this.department);
+    }
+
+//    ASK FOR DEPARTMENT
+    private String setDepartment(){
+        System.out.println("DEPARTMENT CODES:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+        Scanner scan = new Scanner(System.in);
+        int depChoice = scan.nextInt();
+        if (depChoice == 1) {
+            return "sales";
+        }else if (depChoice == 2){
+            return "development";
+        }else if (depChoice == 3){
+            return "accounting";
+        }else{
+            return "";
+        }
     }
 
     //random password generator:
@@ -21,6 +44,10 @@ public class Email {
 
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
     }
 
     public void setFirstName(String firstName) {
@@ -45,10 +72,6 @@ public class Email {
 
     public String getDepartment() {
         return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
     }
 
     public int getMailboxCapacity() {
