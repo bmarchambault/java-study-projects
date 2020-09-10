2cfb39c5949d83399db94ccf9c0e320f3cfa1a0d
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-10 18:07:24 -0500

    calling all methods

--- src/studentDatabaseApp/StudentDatabaseApp.java
+++ src/studentDatabaseApp/StudentDatabaseApp.java
@@ -5,8 +5,9 @@ public class StudentDatabaseApp {
         Student student1 = new Student();
         //ask how many new students to add
         student1.enroll();
-
-
+        student1.viewTuitionBalance();
+        student1.payTuition();
+        System.out.println(student1.showInfo());
 
     }
 }
