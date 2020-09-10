053e67d6db396a93134e14a801bdcb4ac7902c3f
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-10 15:44:52 -0500

    created a new student object

--- src/studentDatabaseApp/StudentDatabaseApp.java
+++ src/studentDatabaseApp/StudentDatabaseApp.java
@@ -2,6 +2,10 @@ package studentDatabaseApp;
 
 public class StudentDatabaseApp {
     public static void main(String[] args) {
+        Student student1 = new Student();
+        //ask how many new students to add
+
+
 
     }
 }
