7f1e73ae8e23aebed152dd1b9b8b976c5de3c9ef
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-15 12:15:37 -0500

    testing contstructors

--- src/bankAcctApp/BankAccountApp.java
+++ src/bankAcctApp/BankAccountApp.java
@@ -1,4 +1,10 @@
 package bankAcctApp;
 
 public class BankAccountApp {
+    //read a csv file and create accounts based on that data
+Checking cAcct1 = new Checking("Tom Wilson", "555992222", 1500);
+
+Savings sAcct1 = new Savings("Bob Dillon", "999448888", 2500);
+
+
 }
