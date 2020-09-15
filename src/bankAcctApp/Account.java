1526f259a37680bc6031da4a0c8e78f2c9ca1c23
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-15 12:14:57 -0500

    created super constructor

--- src/bankAcctApp/Account.java
+++ src/bankAcctApp/Account.java
@@ -3,15 +3,21 @@ package bankAcctApp;
 public abstract class Account implements IBaseRate {
     //cant create object from here
     //list common properties for savings and checking accounts
-String firstName;
-String lastName;
-int accountNumber;
-int SSN;
-int fiveDigitRandom;
-int threeDigitRandom;
-
+//String firstName;
+//String lastName;
+String accountNumber;
+String sSN;
+double balance;
+double rate;
+//String name = firstName + " " + lastName;
+    String name;
     //constructor to set base properties and initialize the account
-
+public Account (String name, String sSN, double initDeposit){
+   this.name = name;
+   this.sSN = sSN;
+   balance = initDeposit;
+    System.out.println("name: " + name + " SSN: "+   sSN + " Balance: "+   balance);
+}
     //list common methods
 
 }
