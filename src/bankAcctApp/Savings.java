6f5a24001daaba863a020f1da63e60c6ae38e9ab
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-15 12:16:14 -0500

    calling super

--- src/bankAcctApp/Savings.java
+++ src/bankAcctApp/Savings.java
@@ -3,13 +3,19 @@ package bankAcctApp;
 public class Savings extends Account {
 
     //list properties are specific to a savings account
-int safetyDepositBox;
-int codeSDB;
+int safetyDepositBoxID;
+int safetyDepositBoxKey;
     //constructor to initialize the savings account properties
 
-    public Savings (int safetyDepositBox, int codeSDB){
-        this.safetyDepositBox = safetyDepositBox;
-        this.codeSDB = codeSDB;
+    public Savings ( String name, String sSN, double initDeposit){
+        super(name, sSN, initDeposit);
+        System.out.println("new savings account");
+
+    }
+
+    @Override
+    public int baseRate() {
+        return 15;
     }
 
     //list methods specific
