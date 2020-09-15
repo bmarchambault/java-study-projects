0ce3fe15718642d7288318c14ede44ecba01d62c
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-15 12:30:43 -0500

    created a method that genterated an account number

--- src/bankAcctApp/Account.java
+++ src/bankAcctApp/Account.java
@@ -11,13 +11,31 @@ double balance;
 double rate;
 //String name = firstName + " " + lastName;
     String name;
+static int index = 10000;
+
+
     //constructor to set base properties and initialize the account
 public Account (String name, String sSN, double initDeposit){
    this.name = name;
    this.sSN = sSN;
    balance = initDeposit;
-    System.out.println("name: " + name + " SSN: "+   sSN + " Balance: "+   balance);
+    System.out.println("NAME: " + name + " SSN: "+   sSN + " BALANCE: "+   balance);
+    //*****set account number:*******
+    //increase index each time an account is created
+    index++;
+    //call this method
+    this.accountNumber = setAcctNum();
+    System.out.println("ACCOUNT NUMBER: " + this.accountNumber);
 }
     //list common methods
+    //1. account number
+    private String setAcctNum(){
+    //grab last two number of the SSN
+    String lastTwoOfSSN = sSN.substring(sSN.length()-2, sSN.length() );
+    //grab the increased index number created by the constructor
+    int uniqueID = index;
+    int randomNum = (int) (Math.random() * Math.pow(10, 3));
+            return lastTwoOfSSN + uniqueID + randomNum;
+    }
 
 }
