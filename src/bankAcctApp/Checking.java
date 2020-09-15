dbd777c4b1c7f4371204aad5e35c28828ee7f535
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-15 12:16:03 -0500

    calling super

--- src/bankAcctApp/Checking.java
+++ src/bankAcctApp/Checking.java
@@ -5,8 +5,19 @@ public class Checking extends Account{
         //debit card
         //pin etc...
 int debitCardNum;
-int pin;
+int debitCardPin;
 
+
+public Checking(String name, String sSN, double initDeposit){
+    super(name, sSN, initDeposit);
+    System.out.println("new checking account");
+    System.out.println("Name: " + name);
+}
+
+    @Override
+    public int baseRate() {
+        return 0;
+    }
 
 
     //constructor to initialize the checking account properties
