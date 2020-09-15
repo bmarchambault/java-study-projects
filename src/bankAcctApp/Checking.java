94dd70aaf2f31f37f5ff5f853ca6f85397ed4525
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-15 13:34:00 -0500

    created a method that generates a debit card number and a pin

--- src/bankAcctApp/Checking.java
+++ src/bankAcctApp/Checking.java
@@ -4,20 +4,30 @@ public class Checking extends Account{
     //list properties are specific to a checking account
         //debit card
         //pin etc...
-int debitCardNum;
+long debitCardNum;
 int debitCardPin;
 
 
 public Checking(String name, String sSN, double initDeposit){
     super(name, sSN, initDeposit);
     accountNumber = "2" + accountNumber;
-
+    setDebitCard();
 }
+    private void setDebitCard(){
+        //casting as an in truncates the random number.
+        debitCardNum = (long) (Math.random() * Math.pow(10, 12));
+        debitCardPin = (int) (Math.random() * Math.pow(10, 4));
+    }
 
     @Override
     public void showInfo() {
+    System.out.println("ACCOUNT TYPE: Checking");
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: Checking");
+        System.out.println(
+                "Your Checking Account Features"+
+                        "\n   Debit Card Number: " + debitCardNum +
+                        "\n   Pin: " + debitCardPin
+        );
     }
 
     @Override
