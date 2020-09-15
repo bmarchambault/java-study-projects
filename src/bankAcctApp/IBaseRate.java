f9f4f7b8f243ba13bf1613891e4ae802348616ef
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-15 13:52:45 -0500

    created base rate api

--- src/bankAcctApp/IBaseRate.java
+++ src/bankAcctApp/IBaseRate.java
@@ -1,7 +1,13 @@
 package bankAcctApp;
 
 public interface IBaseRate {
-// write a method that returns a base rate
- public int baseRate();
-
+ // write a method that returns a base rate
+ //this rate would normally be defined on an outside source or database.  real world, I'd have to reach out and get that number
+ //this is a hard coded benchmark rate simulating the above.
+ default double getBaseRate() {
+  return 2.5;
+ }
 }
+// this is an api
+//
+//}
