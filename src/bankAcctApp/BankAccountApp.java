299a0b2fafeec08025617ee48f8efea0049c0912
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-15 17:41:54 -0500

    main can now create accounts based on csv file

--- src/bankAcctApp/BankAccountApp.java
+++ src/bankAcctApp/BankAccountApp.java
@@ -2,10 +2,15 @@ package bankAcctApp;
 
 import jdk.swing.interop.SwingInterOpUtils;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
     public static void main(String[] args) {
+        //data structure for the objects:
+        List<Account> accounts = new LinkedList<Account>();
+        //add elements based on account type in the method below
+
 //testing:
        // Checking cAcct1 = new Checking("Tom Wilson", "555992222", 1500);
 //        Savings sAcct1 = new Savings("Bob Dillon", "999448888", 2500);
@@ -20,27 +25,35 @@ public class BankAccountApp {
         //this file could be located anywhere.
         String file = "src/files/NewBankAccounts.csv";
         List<String[]> newAccountHolders = utilities.CSV.read(file);
-        for (String[] accountHolder : newAccountHolders){
+        for (String[] accountHolder : newAccountHolders) {
 
             String name = accountHolder[0];
             String sSn = accountHolder[1];
             String accountType = accountHolder[2];
             double initDeposit = Double.parseDouble(accountHolder[3]);
-            System.out.println(name + " " + sSn + " " + accountType + " $" +  initDeposit);
+            System.out.println(name + " " + sSn + " " + accountType + " $" + initDeposit);
 
-            if (accountType.equalsIgnoreCase("Savings")){
+            if (accountType.equalsIgnoreCase("Savings")) {
                 System.out.println("Open a  SAVINGS");
-            }else if(accountType.equalsIgnoreCase("checking")){
+                accounts.add(new Savings(name, sSn, initDeposit));
+            } else if (accountType.equalsIgnoreCase("checking")) {
                 System.out.println("Open a  CHECKING ACCOUNT");
-            }else{
+                accounts.add(new Checking(name, sSn, initDeposit));
+            } else {
                 System.out.println("ERROR READING ACCOUNT TYPE");
             }
-
-
-//            System.out.println(accountHolder[1]);
-//            System.out.println(accountHolder[2]);
-//            System.out.println(accountHolder[3]);
-
         }
+//        accounts.get(5).showInfo();//testing an element
+
+    //show info for each account created.
+        for (Account acct : accounts) {
+            System.out.println("\n***************");
+            acct.showInfo();
+        }
+//EXPERIMENT WITH WHAT YOU CAN DO.
+accounts.get((int)Math.random()* accounts.size()).deposit(100);
+
+
+
     }
 }
